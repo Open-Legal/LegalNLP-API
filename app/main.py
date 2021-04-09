a5dff8046a4b388d7feb82147e6bf8d40acb12aa
Ryan McDonough <MCDOR@addleshawgoddard.com>
2021-04-09 15:44:12 +0100

    Update main.py
    Add support for other endpoints.

--- app/main.py
+++ app/main.py
@@ -15,13 +15,41 @@ def read_root():
     return {"Status": "Working"}
 
 @app.post("/act")
-def read_item(req: Request):
+def Act(req: Request):
     return JSONResponse(content=jsonable_encoder(lexnlp.extract.en.acts.get_act_list(req.text)))
 
 @app.post("/amount")
-def read_item(req: Request):
+def Amount(req: Request):
     return JSONResponse(content=jsonable_encoder(list(lexnlp.extract.en.amounts.get_amounts(text,True))))
 
 @app.post("/constraint")
-def read_item(req: Request):
+def Constraint(req: Request):
     return JSONResponse(content=jsonable_encoder(lexnlp.extract.en.acts.get_act_list(req.text)))
+
+@app.post("/company")
+def Company(req: Request):
+    return JSONResponse(content=jsonable_encoder((list(lexnlp.extract.en.entities.nltk_re.get_entities.nltk_re.get_companies(text)))))
+
+@app.post("/date")
+def Date(req: Request):
+    return JSONResponse(content=jsonable_encoder(list(lexnlp.extract.en.dates.get_dates(text))))
+
+@app.post("/definition")
+def Definition(req: Request):
+    return JSONResponse(content=jsonable_encoder(list(lexnlp.extract.en.definitions.get_definitions(text,True,True))))
+
+@app.post("/duration")
+def Duration(req: Request):
+    return JSONResponse(content=jsonable_encoder(list(lexnlp.extract.en.durations.get_durations(text))))
+
+@app.post("/money")
+def Money(req: Request):
+    return JSONResponse(content=jsonable_encoder(list(lexnlp.extract.en.money.get_money(text,True,4))))
+
+@app.post("/regulation")
+def Regulation(req: Request):
+    return JSONResponse(content=jsonable_encoder(list(lexnlp.extract.en.regulations.get_regulations(text,True,True))))
+
+@app.post("/trademark")
+def Regulation(req: Request):
+    return JSONResponse(content=jsonable_encoder(list(lexnlp.extract.en.trademarks.get_trademarks(text))))
